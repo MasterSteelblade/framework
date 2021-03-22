59715131829c2d87f790b3f688fa1fbaff4021c4
MasterSteelblade <mastersteelblade@protonmail.com>
2021-03-22 13:36:28 +0000

    Initial

---
+++ src/Validator.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace Steelblade\Framework;
+
+/**
+ * Class to provide basic validation of things. 
+ */
+
+ class Validator {
+
+    public static function colour(string $input):bool {
+        if (preg_match('/^[A-Fa-f0-9]{6}$/', $input) !== 0) {
+            $matched = true;
+        } else {
+            $matched = false;
+        }
+        return $matched;
+    }
+
+    /** For the Americans. */
+    public static function color(string $input):bool {
+        return Steelblade\Framework\Validator::colour($input);
+    }
+
+    public static function username(string $input, array $restricted = array()):bool {
+        if (preg_match('/^[-a-zA-Z0-9_]+$/', $input) !== 0) {
+            $matched = true;
+        } else {
+            $matched = false;
+        }
+        return $matched;
+    }
+    public static function email(string $email):bool {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $matched = true;
+        } else {
+            $matched = false;
+        }
+        return $matched;
+    }
+ }
